46e4a08c69da3f60fd94fb7ff9c9af79ec125958
kateschlagel <kmaillette@gmail.com>
2015-01-24 10:47:54 -0600

    Put copyright symbols in superscript tags.

--- docks.php
+++ docks.php
@@ -1,56 +1,49 @@
-
 <?php include 'header.php'; ?>
 
 <div id="sidenav">
-
-
-		<ul id="buttons">
-			<li><a href="details.php">Details</a></li>
-			<li><a href="accessories.php">Accessories</a></li>
-			<li><a href="instructions.php">Instructions</a></li>
-			<li><a href="order.php">Pricing/Orders</a></li>
-		</ul>
-		
-		<img src="img/docks/free.gif" alt="free deliver in metro area" />
+    <ul id="buttons">
+        <li><a href="details.php">Details</a></li>
+        <li><a href="accessories.php">Accessories</a></li>
+        <li><a href="instructions.php">Instructions</a></li>
+        <li><a href="order.php">Pricing/Orders</a></li>
+    </ul>
+    <img src="img/docks/free.gif" alt="free deliver in metro area"/>
 </div> <!-- end of side_nav -->
-
-
 <div id="main">
-		
-		<div class="main_photo">
-			<img src="img/docks/DockLake2_small.jpg" alt="dock on water" width="218" height="144" />
-			<img src="img/docks/DockShore1.jpg" alt="dock on water"
-			width="214" height="146" />
-			<img src="img/docks/DockZigZag1.jpg" alt="dock on water"
-			width="215" height="148" />
-		</div> 
+    <div class="main_photo">
+        <img src="img/docks/DockLake2_small.jpg" alt="dock on water" width="218" height="144"/>
+        <img src="img/docks/DockShore1.jpg" alt="dock on water"
+             width="214" height="146"/>
+        <img src="img/docks/DockZigZag1.jpg" alt="dock on water"
+             width="215" height="148"/>
+    </div>
+    <h1>Walk A Dock</h1>
 
-	<h1>Walk A Dock</h1>
-		
-		<p>Walk A Dock&copy; is a modular dock system with countless possible configurations and customizability. The large leg pads, light weight construction, and rigidity of the frame allow one person to effortlessly walk each section into position without ever getting into the water. </p>
-		
-		<p>We also sell other brands of docks and boat lifts.  If Walk A Dock&copy; does not fit your needs, we'll find something that will.  <a href="contact.php">Contact us</a> for more information regarding other docks and lifts.</p>
-	
-	<h3>Features</h3>	
-		<ul>
-			<li>One person can install a dock without getting in the water.
-			</li>
-			<li>No component is more than 40 pounds with the exclusion of large wings.
-			</li>
-			<li>All decking is divided into manageable pallets for ease of installation.
-			</li>
-			<li>Sections can stack with accessories inside and decking on top for tidy winter storage.
-			</li>
-			<li>Sections rated to hold up to one ton of weight.
-			</li>
-			<li>Sections connect with sturdy hardware and locking pins.
-			</li>
-			<li>Benches, Ladders and stairs easily and firmly fasten to section frame.
-			</li>			
-		</ul>
-		
-		
+    <p>Walk A Dock<sup>&copy;</sup> is a modular dock system with countless possible configurations and customizability. The large
+        leg pads, light weight construction, and rigidity of the frame allow one person to effortlessly walk each
+        section into position without ever getting into the water. </p>
 
+    <p>We also sell other brands of docks and boat lifts. If Walk A Dock<sup>&copy;</sup> does not fit your needs, we'll find
+        something that will. <a href="contact.php">Contact us</a> for more information regarding other docks and lifts.
+    </p>
+
+    <h3>Features</h3>
+    <ul>
+        <li>One person can install a dock without getting in the water.
+        </li>
+        <li>No component is more than 40 pounds with the exclusion of large wings.
+        </li>
+        <li>All decking is divided into manageable pallets for ease of installation.
+        </li>
+        <li>Sections can stack with accessories inside and decking on top for tidy winter storage.
+        </li>
+        <li>Sections rated to hold up to one ton of weight.
+        </li>
+        <li>Sections connect with sturdy hardware and locking pins.
+        </li>
+        <li>Benches, Ladders and stairs easily and firmly fasten to section frame.
+        </li>
+    </ul>
 </div> <!-- end of main -->
 
 
