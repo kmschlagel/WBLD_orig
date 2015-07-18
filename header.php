2b2e91237be0aec31c94abe2286dd1813caa3cdf
kateschlagel <kmaillette@gmail.com>
2015-07-18 17:03:31 -0500

    Started implementing simple captcha for contact page.

--- header.php
+++ header.php
@@ -1,94 +1,87 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>White Bear Lift and Dock Home</title>
+    <title>White Bear Lift and Dock Home</title>
+    <link rel="stylesheet" href="fonts/merriweather-fontfacekit/stylesheet.css" type="text/css"/>
+    <link rel="stylesheet" href="jquery.simpleCaptcha.css" type="text/css"/>
+    <link rel="stylesheet" href="wbld_style.css" type="text/css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="validate.js"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-32805123-2']);
+        _gaq.push(['_trackPageview']);
 
-<link rel="stylesheet" href="wbld_style.css" type="text/css"/>
-
-<link rel="stylesheet" href="fonts/merriweather-fontfacekit/stylesheet.css" type="text/css"/>
-<script type="text/javascript" src="validate.js"></script>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-32805123-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
-
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+    <script src="jquery.simpleCaptcha.js"></script>
 </head>
-
 <body>
-
 <div id="wrapper">
+    <div id="header">
+        <div id="logo">
+            <a href="index.php"><img src="img/walkadock_logo.jpg" width="206" height="92"/></a>
 
-
-<div id="header">
-	<div id="logo">
-	<a href="index.php"><img src="img/walkadock_logo.jpg" width="206" height="92"/></a>
-	  <div id="fb">
-	  <a href="http://www.facebook.com/pages/White-Bear-Lift-Dock-Inc/353477108006953" target="blank"><img src="img/social/facebook.png" alt="facebook logo" width="30" height="29"></img></a>
-		</div>	
-	</div> <!-- end of logo div -->
-	
-	<div id="slideshow-div" class="rs-slideshow">
-     
-        <div class="slide-container">
-        <img src="slideshow/images/slide5.jpg"
-            alt="Dock on White Bear Lake"/>
-    
-     </div>
-    <ol class="slides">
-        <li>
-            <a href="slideshow/images/slide1.jpg">morzine-2011-a</a>
-        </li>
-        <li>
-            <a href="slideshow/images/slide2.jpg">morzine-2011-a</a>
-        </li>
-        <li>
-            <a href="slideshow/images/slide3.jpg">morzine-2011-a</a>
-        </li>
-        <li>
-            <a href="slideshow/images/slide4.jpg">morzine-2011-a</a>
-        </li>
-        <li>
-            <a href="slideshow/images/slide6.jpg">morzine-2011-a</a>
-        </li>
-        <li>
-            <a href="slideshow/images/slide7.jpg">morzine-2011-a</a>
-        </li>
-        <li>
-            <a href="slideshow/images/slide8.jpg">morzine-2011-a</a>
-        </li>
-    </ol>
-     
-</div> <!-- end slideshow -->
-
-	
-<div id="nav">
-	<p>(651) 653-6476</p>
-	<ul>
-		<li><a href="docks.php" >Docks</a>
-			<ul>
-				<li><a href="details.php">Details</a></li>
-				<li><a href="accessories.php">Accessories</a></li>
-				<li><a href="instructions.php">Instructions</a></li>
-				<li><a href="order.php">Pricing/Order Info</a></li>			
-			</ul>
-		</li>
-		
-		<li><a href="services.php" >Services</a></li>
-		<li><a href="fabrication.php" >Fabrication</a></li>
-		<li><a href="about.php" >About</a></li>
-		<li><a href="contact.php" >Contact</a></li>
-		
-	</ul>
-</div> <!-- end of nav -->
-</div> <!-- end of header -->
+            <div id="fb">
+                <a href="http://www.facebook.com/pages/White-Bear-Lift-Dock-Inc/353477108006953" target="blank"><img
+                        src="img/social/facebook.png" alt="facebook logo" width="30" height="29"></img></a>
+            </div>
+        </div>
+        <!-- end of logo div -->
+        <div id="slideshow-div" class="rs-slideshow">
+            <div class="slide-container">
+                <img src="slideshow/images/slide5.jpg"
+                     alt="Dock on White Bear Lake"/>
+            </div>
+            <ol class="slides">
+                <li>
+                    <a href="slideshow/images/slide1.jpg">morzine-2011-a</a>
+                </li>
+                <li>
+                    <a href="slideshow/images/slide2.jpg">morzine-2011-a</a>
+                </li>
+                <li>
+                    <a href="slideshow/images/slide3.jpg">morzine-2011-a</a>
+                </li>
+                <li>
+                    <a href="slideshow/images/slide4.jpg">morzine-2011-a</a>
+                </li>
+                <li>
+                    <a href="slideshow/images/slide6.jpg">morzine-2011-a</a>
+                </li>
+                <li>
+                    <a href="slideshow/images/slide7.jpg">morzine-2011-a</a>
+                </li>
+                <li>
+                    <a href="slideshow/images/slide8.jpg">morzine-2011-a</a>
+                </li>
+            </ol>
+        </div>
+        <!-- end slideshow -->
+        <div id="nav">
+            <p>(651) 653-6476</p>
+            <ul>
+                <li><a href="docks.php">Docks</a>
+                    <ul>
+                        <li><a href="details.php">Details</a></li>
+                        <li><a href="accessories.php">Accessories</a></li>
+                        <li><a href="instructions.php">Instructions</a></li>
+                        <li><a href="order.php">Pricing/Order Info</a></li>
+                    </ul>
+                </li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="fabrication.php">Fabrication</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <!-- end of nav -->
+    </div>
+    <!-- end of header -->
--- sendmail.php
+++ sendmail.php
@@ -1,31 +1,35 @@
 <?php
-$name = $_POST['name'];
+if (isset($_SESSION['simpleCaptchaAnswer']) && $_POST['captchaSelection'] == $_SESSION['simpleCaptchaAnswer']) {
+    // They're human! Continue processing the rest of the form
+
+    $name = $_POST['name'];
 
 
-$to = 'walkadock@q.com';
-$subject = 'New Customer ';
+    $to = 'walkadock@q.com';
+    $subject = 'New Customer ';
 
-$message = "Name: $name\n";
-$message .= 'Email: ' . $_POST['email'] . "\n";
-$message .= 'City: ' . $_POST['city'] . "\n";
-$message .= 'State: ' . $_POST['state'] . "\n";
-$message .= 'Phone: ' . $_POST['phone'] . "\n";
-$message .= 'Docks: ' . $_POST['docks'] . "\n";
-$message .= 'Lifts: ' . $_POST['boat_lifts'] . "\n";
-$message .= 'Used Docks: ' . $_POST['used_docks'] . "\n";
-$message .= 'Used Lifts: ' . $_POST['used_lifts'] . "\n";
-$message .= 'Services: ' . $_POST['services'] . "\n";
-$message .= 'Welding/Fabrication: ' . $_POST['welding_fabrication'] . "\n";
-$message .= 'Other: ' . $_POST['other'] . "\n";
-$message .= 'Other Information: ' . $_POST['other_info'] . "\n";
-$message .= "\n";
-
-
-$headers =	'From: <' . $_POST['email'] . '> '. $name ."\r\n" .
-			'Reply-To: <' . $_POST['email'] . '> '. $name . "\r\n" .
-			'X-Mailer: PHP/' . phpversion();
+    $message = "Name: $name\n";
+    $message .= 'Email: ' . $_POST['email'] . "\n";
+    $message .= 'City: ' . $_POST['city'] . "\n";
+    $message .= 'State: ' . $_POST['state'] . "\n";
+    $message .= 'Phone: ' . $_POST['phone'] . "\n";
+    $message .= 'Docks: ' . $_POST['docks'] . "\n";
+    $message .= 'Lifts: ' . $_POST['boat_lifts'] . "\n";
+    $message .= 'Used Docks: ' . $_POST['used_docks'] . "\n";
+    $message .= 'Used Lifts: ' . $_POST['used_lifts'] . "\n";
+    $message .= 'Services: ' . $_POST['services'] . "\n";
+    $message .= 'Welding/Fabrication: ' . $_POST['welding_fabrication'] . "\n";
+    $message .= 'Other: ' . $_POST['other'] . "\n";
+    $message .= 'Other Information: ' . $_POST['other_info'] . "\n";
+    $message .= "\n";
 
 
-mail($to, $subject, $message, $headers);
-header('location: http://www.walkadock.com/thankyou.php');
+    $headers = 'From: <' . $_POST['email'] . '> ' . $name . "\r\n" .
+        'Reply-To: <' . $_POST['email'] . '> ' . $name . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+
+    mail($to, $subject, $message, $headers);
+    header('location: http://www.walkadock.com/thankyou.php');
+}
 ?>
